1eba62307cd7e99dc4f6c1de45ef90ed98683b76
fbk-rena <gaby.ranarz@gmail.com>
2017-04-10 08:57:21 -0500

    corren 3 pruebas

---
+++ ejercicio-01.js
@@ -0,0 +1,17 @@
+function isValidCard(creditCardNumber) {
+    // Escribe tu código aquí
+    if(creditCardNumber=== undefined){
+      return "Ingresa un numero";
+    }
+    if(typeof(creditCardNumber) !== "number" ){
+      return "Error de dato";
+    }
+    if(creditCardNumber.toString().length !== 16){
+      return "Faltan numeros";
+    }
+}
+
+// NO TOCAR ESTE CÓDIGO O EXPLOTARÁ LA PC EN 10 SEGUNDOS
+if (typeof exports !== 'undefined') {
+    exports.isValidCard = isValidCard;
+}
